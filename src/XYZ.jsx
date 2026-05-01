55a85d5cf92dde7798aff1d2e8388ebe289ba043
AditiKesharwani <hiaditii2001@gmail.com>
2026-05-01 16:44:54 +0530

    Practiced api calling via redux-tolkit and also done caching

--- src/XYZ.jsx
+++ src/XYZ.jsx
@@ -1,17 +1,22 @@
 
-import { useEffect, useState } from "react";
-
+import {  useState } from "react";
+import { useGetPostsByPageQuery } from "./services/api";
 function XYZ() {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  // const[cache,setCache]=useState({})
+  const {data:users=[],isFetching }= useGetPostsByPageQuery(currentPage)
+  // const itemsPerPage=10
 
-  const itemsPerPage=10
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${itemsPerPage}`)
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, [currentPage]);
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${itemsPerPage}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setUsers(data));
+  //     // setCache((prev)=> ({
+  //     //   ...prev,
+  //     //   [currentPage]:data
+  //     // }))
+  // }, [currentPage]);
 
   return (
     <div>
@@ -23,6 +28,8 @@ function XYZ() {
           {user.id} - {user.title}
         </p>
       ))}
+
+      {isFetching && <p>...loading</p>}
 
       <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
         Prev
